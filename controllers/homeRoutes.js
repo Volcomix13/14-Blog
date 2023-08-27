bf57afb9d426057407ac016a71217b4b423bd60a
Volcomix <m.santo13@hotmail.com>
2023-08-27 10:07:25 -0600

    added routes to homeRoutes

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -13,7 +13,7 @@ router.get('/', async (req, res) => {
     ],
     include: [{
       model: Comment,
-      attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+      attributes: ['id', 'comment', 'post_id', 'user_id', 'created_at'],
       include: {
         model: User,
         attributes: ['username']
@@ -34,3 +34,52 @@ router.get('/', async (req, res) => {
       res.status(500).json;
     });
 });
+
+router.get('/post/:id', async (req, res) => {
+  Post.findOne({
+      where:{
+        id: req.params.id,
+      },
+      attributes: ['id', 'comment', 'post_id', 'user_id', 'created_at'],
+      include: {
+        model: User,
+        attributes: ['username']
+      }
+  })
+    .then(dbPostData => {
+      const post = dbPostData.map(post => post.get({
+        plain: true
+      }));
+      res.render('single-post', {
+        posts,
+        loggedIn: req.session.loggedIn
+      });
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json;
+    });
+});
+
+
+router.get('/signup', (req, res) => {
+  // If the user is already logged in, redirect the request to another route
+  if (req.session.logged_in) {
+    res.redirect('/');
+    return;
+  }
+
+  res.render('signup');
+});
+
+router.get('/login', (req, res) => {
+  // If the user is already logged in, redirect the request to another route
+  if (req.session.logged_in) {
+    res.redirect('/');
+    return;
+  }
+
+  res.render('login');
+});
+
+module.exports = router;
