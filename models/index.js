f85f3de206f01173164b8aa72fbe18a657a72d99
Volcomix <m.santo13@hotmail.com>
2023-08-27 18:28:32 -0600

    updated relationshipts in models/ index.js

--- models/index.js
+++ models/index.js
@@ -2,13 +2,28 @@ const User = require('./user');
 const Comment = require('./comment');
 const Post = require ('./post')
 
-User.hasMany(Comment, {
+User.hasMany(Post, {
   foreignKey: 'user_id',
-  onDelete: 'CASCADE'
 });
 
-Comment.belongsTo(User, {
+User.hasMany(Comment,{
+  foreignKey: "user_id",
+});
+
+Post.belongsTo(User, {
   foreignKey: 'user_id'
+});
+
+Post.hasMany(Comment,{
+  foreignKey: "post_id",
+});
+
+Comment.belongsTo(Post, {
+  foreignKey: "post_id",
+});
+
+Comment.belongsTo(User,{
+  foreignKey: "user_id"
 });
 
 module.exports = { User, Comment, Post };
